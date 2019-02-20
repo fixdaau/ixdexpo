bdd73ee7b7abfa5dbd398cee298b6a17cd65c2a0
Unknown <rjacob15@student.aau.dk>
2019-02-20 20:26:36 +0100

    Added initial routing for project

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.scss';
 import H1 from '../Components/H1/H1';
+import { Route } from 'react-router-dom';
+import Main from '../Pages/Main';
+import CompanySignup from '../Pages/CompanySignup';
 
 class App extends Component {
   render() {
     return (
-      <H1>First Title Component</H1>
+      <div>
+        <Route exact path='/' component={Main} />
+        <Route exact path='/company-signup' component={CompanySignup} />
+      </div>
     );
   }
 }
---
+++ src/Pages/CompanySignup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CompanySignup extends Component {
+    render() {
+        return (
+            <div>
+                Company Signup
+            </div>
+        );
+    }
+}
+
+export default CompanySignup;
