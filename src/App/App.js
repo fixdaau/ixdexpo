516f302539f0a3f4404f8895063ee1873b0471ba
Tobias <tobias_seindal@hotmail.com>
2019-02-20 20:23:05 +0100

    Added AirBnB styleguide with more rules for eslint

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
 import H1 from '../Components/H1/H1';
 
-class App extends Component {
-  render() {
-    return (
-      <H1>First Title Component</H1>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return <H1>First Title Component</H1>;
+//   }
+// }
+// Jeg har bare lige lavet App om til en funktion hurtigt, så airbnb formatet holder kæft for nu
+
+const App = () => <H1>First Title Component</H1>;
 
 export default App;
-
-
