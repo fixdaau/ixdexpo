51092e2d69795a4df59e7f81a76219b4da6fa8af
Anders Høgh <ahha15@student.aau.dk>
2019-02-23 23:10:41 +0100

    Footer
    Added react grid to footer comp. Cleaned up css as well.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Footer.scss';
 import HR from '../HR/HR';
+import { Row, Col, Container } from 'react-grid-system';
 
 const Footer = () => {
     return (
-        <div>
+        <Container>
             <HR></HR>
-            <div className='bottomText'>
-                <p >FixD © 2019</p>
-                <div className='endText'>
+            <Row>
+                <Col xs={7}>
+                    <p>FixD © 2019</p>
+                </Col>
+                <Col xs={2}>
                     <p>Expo team</p>
+                </Col>
+                <Col xs={3}>
                     <p>ixd-expo@fixd.aau.dk</p>
-                </div>
-            </div>
-            
-        </div>
+                </Col>
+            </Row>
+        </Container>
     );
-};
-
-
+}
 export default Footer;
 
 
