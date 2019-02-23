a924f29a7b6883d260d86f32ed42fd24d7cc86da
Anders Høgh <ahha15@student.aau.dk>
2019-02-23 23:19:14 +0100

    h3 git error

---
+++ src/Components/H3/H3.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './H3.scss';
+
+const H3 = (props) => {
+    return (
+        <div>
+            <h3 className='h3'>{props.children}</h3>
+        </div>
+    );
+};
+
+export default H3;
