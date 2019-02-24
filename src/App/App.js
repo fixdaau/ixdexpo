e670fb701a3795b9556247691e2a282a20d8c760
Unknown <rjacob15@student.aau.dk>
2019-02-24 19:44:22 +0100

    Auto stash before merge of "feature/companyPage" and "development"

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { Col, Container, Row } from 'react-grid-system';
 import H1 from '../Components/H1/H1';
 import { Route } from 'react-router-dom';
 import Main from '../Pages/Main';
@@ -17,5 +18,3 @@ class App extends Component {
 }
 
 export default App;
-
-
---
+++ src/Sections/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+
+const Footer = () => {
+    return (
+        <Container class='footer'>
+            <Row>
+                <Col xs={12} md={8}>
+
+                </Col>
+                <Col xs={12} md={8}>
+                    
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Footer;
