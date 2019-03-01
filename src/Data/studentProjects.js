f83cb1ab2d63248da2a7d006c29b8a8d8eea666b
Unknown <rjacob15@student.aau.dk>
2019-03-01 13:08:23 +0100

    Removed duplicate projectse

--- src/Data/studentProjects.js
+++ src/Data/studentProjects.js
@@ -18,15 +18,5 @@ export const getProjects = () => ([
         imgSrc: 'Tempus1.png',
         title: 'Tempus',
         description: 'An interactive physical time managing tool for streamlining meetings through dynamic visualisation of time.'
-    },
-    {
-        imgSrc: 'bellis.jpg',
-        title: 'Bellis',
-        description: 'Helping plant owners not killing their plants, while providing better indoor-climate using plants own self-cleaning capabilities.'
-    },
-    {
-        imgSrc: 'eia.jpg',
-        title: 'EIA',
-        description: 'An ML-based personal assistant for a Medical Secretary designed and developed to augment trivial tasks in a Hospital Ward.'
-    },
+    }
 ])
