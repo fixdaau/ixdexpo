8f45d5e4c652d2cea93a134087e74adb3ccddcf2
Rune <rjacob15@student.aau.dk>
2020-02-19 20:15:52 +0100

    Fixing typos in tie schedule description

--- src/Data/timeScheduleData.js
+++ src/Data/timeScheduleData.js
@@ -4,27 +4,27 @@ export const getTimeScheduleData = () => [
     title: "Welcome & Introduction",
     imgSrc: "./assets/images/welcome.jpg",
     description:
-      "The first point on the agenda is a formal welcome, and introduction of the event. We will present the programme for the day and what you are going to experience."
+      "The first point on the agenda is a formal welcome and introduction of the event. We will present the programme for the day and what you will experience!"
   },
   {
     timeSpan: "13:15 - 15:00",
     title: "Explore the EXPO Stands",
     imgSrc: "./assets/images/stands.jpg",
     description:
-      "Meet the designers of tomorrow! They are eager to present their project and they are prepared to tap into the challenges of you company. They will be hosts presentations of their projects from both Bachelor’s and Master’s educations."
+      "Meet the designers of tomorrow! We are eager to present our projects and we are prepared to tap into the challenges of you company. We will present our projects from both the Bachelor’s and Master’s educations of Interaction Design"
   },
   {
     timeSpan: "15:00 - 16:00",
     title: "Panel + Exquisite Food",
     imgSrc: "./assets/images/food.jpg",
     description:
-      "We have invitied comapnies to discuss the value of having interaction designers in-house. These comapnies hired both student workers and full-time interaction designers."
+      "We have invited company leaders to discuss the value of having interaction designers in-house. These companies have hired both student workers and full-time interaction designers."
   },
   {
     timeSpan: "16:00 - 19:00",
     title: "Networking & Cocktails",
     imgSrc: "./assets/images/networking.jpg",
     description:
-      "It time for refreshments! To finish the day we will round up with a serving of cocktails that will accompany you while networking with students and other attendees. Who know’s you might meet you next colleague."
+      "It is time for refreshments! To finish the day we will round up with a serving of cold drinks accompaniyng you while networking with students and other attendees. Who know.. you might meet you next colleague!"
   },
 ];
