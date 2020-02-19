9c8c85aa6c7a56aeaca96d031e1083938d74e75b
Rune <rmj@tiatechnology.com>
2020-02-19 20:14:23 +0100

    What is IxD Section
    Stylet den om, lagt den ind i grid. Importeret Feather-Icons-react og tilføjet ikoner, det eneste som mangler er styling af header.

--- src/Sections/InteractionDesignSection/InteractionDesignSection.js
+++ src/Sections/InteractionDesignSection/InteractionDesignSection.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Row, Col, Visible } from 'react-grid-system';
 import './InteractinoDesginSection.scss'
-import H1 from'../../Components/H1/H1'
-import H3 from'../../Components/H3/H3'
+import H2 from'../../Components/H2/H2';
+import H3 from'../../Components/H3/H3';
 import Card from '../../Components/Card/Card';
+import FeatherIcons from 'feather-icons-react';
 
 const InteractionDesignSection = () => {
+    const feather = require('feather-icons')
     return (
         <div className="ixdbg">
+            <container fluid>
             <Row>
-                <Col xs={12} debug align='center'>
-                    <H1>What is IxD EXPO?</H1>
+                <Col xs={12} align='center'>
+                    <H2>What is IxD EXPO?</H2>
                 </Col>
             </Row>
 
-            <Row>
-                <Col xs={12}>
+            <Row justify='center'>
+                <Col xs={12} md={8} lg={6}>
                     <p>
                     IxD EXPO is an event where we as Interaction Design students, want to show what we can contribute with at your company. 
                     We will impress you with prototypes, demos and presenting our bachelor and master projects.
@@ -23,25 +26,23 @@ const InteractionDesignSection = () => {
                 </Col>
             </Row>
 
-            <Row>
-                <Col xm={6} md={4}>
-                    <H3>
-                        Indsæt nummer her 1
-                    </H3>
+            <Row justify='around'>
+                <Col xm={3} md={4} lg={3} align='center'>
+                    <H3>61</H3>
                     <p>
                         Students will be eager to meet you.
                     </p>
                 </Col>
 
-                <Col xm={6} md={4} debug>
-                    <H3>Indsæt nummer her 2</H3>
+                <Col xm={6} md={4} lg={3} align='center'>
+                    <H3>18</H3>
                     <p>
                         Companies will be attending.
                     </p>
                 </Col>
 
-                <Col xm={6} md={4}>
-                    <H3>Indsæt Nummer her 3</H3>
+                <Col xm={6} md={4} lg={3} align='center'>
+                    <H3>22</H3>
                     <p>
                         Projects await your intrest.
                     </p>
@@ -54,14 +55,17 @@ const InteractionDesignSection = () => {
                 </Col>
             </Row>
 
-            <Row>
-                <Col xs={6}>
-                    <p>March 20, 2020</p><br/> <p> 13:00-19:00</p>
+            <Row justify='center'>
+                <Col xs={12} md={4} lg={4} align='center'>
+                    <FeatherIcons icon = 'calendar' size='40' color='white'/>
+                    <p>March 20, 2020<br/>13:00-19:00</p>
                 </Col>
-                <Col xs={6}>
-                    <p>Selma Lagerløfs Vej 300,</p><br/><p>9000 Aalborg</p>
+                <Col xs={12} md={4} lg={4} align='center'>
+                    <FeatherIcons icon = 'map-pin' size='40' color='white'/>
+                    <p>Selma Lagerløfs Vej 300,<br/>9000 Aalborg</p>
                 </Col>
             </Row>
+            </container>
         </div>
         // <Row>
         //     <Col xs={12} sm={12} md={6}>
